f87582fd90bd5c2a69aa7842f4d198b0f580d86d
lenovo <markwang@aoji.cn>
2019-04-02 22:16:44 +0800

    order and user html

--- page/order/orderList.js
+++ page/order/orderList.js
@@ -15,15 +15,15 @@ layui.use(['table', 'laydate'], function () {
         url: '../../json/logs.json',
         cellMinWidth: 95,
         page: true,
-        height: "full-20",
+        height: "full-125",
         limit: 20,
         limits: [10, 15, 20, 25],
         id: "systemLog",
         cols: [[
-            {field: 'orderId', title: '订单号', width: 150, align: "center"},
-            {field: 'hasCar', title: '已分配车辆', width: 130},
+            {field: 'orderId', title: '订单号', width: '10%', align: "center"},
+            {field: 'hasCar', title: '已分配车辆', width: '10%'},
             {
-                field: 'driverTel', title: '已分配司机联系方式', width: 150, align: 'center', templet: function (d) {
+                field: 'driverTel', title: '已分配司机联系方式', width: '15%', align: 'center', templet: function (d) {
                     if (d.driverTel.toUpperCase() == "GET") {
                         return '<span class="layui-blue">' + d.driverTel + '</span>'
                     } else {
@@ -31,14 +31,14 @@ layui.use(['table', 'laydate'], function () {
                     }
                 }
             },
-            {field: 'leaseTel', title: '租车人联系电话', align: 'center',  width: 150},
+            {field: 'leaseTel', title: '租车人联系电话', align: 'center', width: '15%'},
             {
-                field: 'orderFee', title: '订单金额', align: 'center', width: 120, templet: function (d) {
+                field: 'orderFee', title: '订单金额', align: 'center', width: '10%', templet: function (d) {
                     return '<span class="layui-btn layui-btn-normal layui-btn-xs">' + d.orderFee + '</span>'
                 }
             },
             {
-                field: 'delayFee', title: '延时费用', align: 'center', width: 120, templet: function (d) {
+                field: 'delayFee', title: '延时费用', align: 'center', width: '10%', templet: function (d) {
                     if (d.delayFee == "正常") {
                         return '<span class="layui-btn layui-btn-green layui-btn-xs">' + d.delayFee + '</span>'
                     } else {
@@ -47,16 +47,16 @@ layui.use(['table', 'laydate'], function () {
                 }
             },
             {
-                field: 'pickCode', title: '取车码', align: 'center', width: 130, templet: function (d) {
+                field: 'pickCode', title: '取车码', align: 'center', width: '5%', templet: function (d) {
                     return '<span class="layui-btn layui-btn-normal layui-btn-xs">' + d.pickCode + '</span>'
                 }
             },
             {
-                field: 'applyTime', title: '用车时间', align: 'center', width: 150, templet: function (d) {
+                field: 'applyTime', title: '用车时间', align: 'center', width: '10%', templet: function (d) {
                     return '<span class="layui-btn layui-btn-normal layui-btn-xs">' + d.applyTime + '</span>'
                 }
             },
-            {title: '操作', width:150, templet:'#orderListBar',fixed:"right",align:"center"}
+            {title: '操作', width: '10%', templet: '#orderListBar', fixed: "right", align: "center"}
         ]]
     });
 
